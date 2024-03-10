1c20535c284edc53fe9e163b6ee4d105b62c6e43
Santos van_der_Wansem <srrvdwansem@gmail.com>
2024-03-11 00:34:38 +0100

    update

---
+++ src/js/index.js
@@ -0,0 +1,117 @@
+import data from '../data.json' assert { type: 'json' };
+
+const year = new Date().getFullYear();
+const birthYear = new Date().getMonth() >= 6 ? 2007 : 2008;
+const age = year - birthYear;
+
+document.querySelector('.year_js').innerHTML = `${year}`;
+document.querySelector('.birth_year_js').innerHTML = `${birthYear}`;
+document.querySelector('.age').innerHTML = `${age}`;
+
+const stack = data.info.stack;
+const stackElement = document.querySelector('#header-tags');
+
+stack.map((tag) => {
+    stackElement.innerHTML += `<span class='header-tag'>${tag}</span>`;
+});
+
+const projects = data.projects;
+const projectsElement = document.querySelector('#projects');
+
+// Loop over an object
+const objectMap = (obj) => Object.fromEntries(Object.entries(obj).map((project) => addCard(project)));
+
+function addCard(project) {
+    const language = project[1].tags[0];
+    let color = '';
+    switch (language) {
+        case 'html':
+            color = 'red';
+            break;
+        case 'js':
+            color = 'yellow';
+            break;
+        case 'css':
+            color = 'purple';
+            break;
+        case 'scss':
+            color = 'pink';
+            break;
+        case 'typescript':
+            color = 'blue';
+            break;
+        case 'python':
+            color = 'blue';
+            break;
+        case 'php':
+            color = 'purple';
+            break;
+        case 'c':
+            color = 'grey';
+            break;
+        case 'jupiter notebook':
+            color = 'orange';
+            break;
+        case 'c++':
+            color = 'pink';
+            break;
+        case 'vue':
+            color = 'green';
+            break;
+        case 'java':
+            color = 'red';
+            break;
+        case 'rust':
+            color = 'grey';
+            break;
+    }
+
+    let urlIcon = '';
+
+    if (project[1].document) {
+        urlIcon = `<a href='${project[1].url}' target='_blank' rel='noreferrer'>
+            <i class='bi bi-file-earmark-text'></i>
+        </a>`;
+    } else {
+        urlIcon = `<a href='${project[1].url}' target='_blank' rel='noreferrer'>
+            <i class='bi bi-box-arrow-up-right'></i>
+        </a>`;
+    }
+
+    let githubIcon = '';
+
+    if (project[1].github !== '') {
+        githubIcon = `<a href='${project[1].github}' target='_blank' rel='noreferrer'>
+                <i class='bi bi-github'></i>
+            </a>`;
+    }
+
+    projectsElement.innerHTML += `<section class='project-card' id='${project[0]}_card'>
+        <div class='card'>
+            <div class='card-img'>
+                <img src='${project[1].image}' id='${project[0]}_img' alt='Website preview' />
+            </div>
+            <div class='card-text'>
+                <span class='card-title'>
+                    <h5>${project[1].naam}</h5>
+                </span>
+                <span class='card-desc'>
+                    <p class='description'>${project[1].beschrijving}</p>
+                </span>
+                <span class='card-tags'>
+                    <p class='lang-tag' id='${color}'>
+                        ${project[1].tags[0]}
+                    </p>
+                    ${project[1].tags[1]},
+                    ${project[1].tags[2]}
+                </span>
+            </div>
+        </div>
+        <div class='project-links'>
+            ${githubIcon}
+            ${urlIcon}
+        </div>
+    </section>`;
+}
+
+objectMap(projects);
