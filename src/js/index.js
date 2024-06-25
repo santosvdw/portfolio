d9192c198804e148002f4105c056a64f76013451
santos <96233951+santosvdw@users.noreply.github.com>
2024-06-25 17:28:04 +0300

    Actualizar index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const birthYear = new Date().getMonth() >= 5 ? 2007 : 2008;
 const age = year - birthYear;
 
 document.querySelector('.year_js').innerHTML = `${year}`;
-document.querySelector('.birth_year_js').innerHTML = `${birthYear}`;
+document.querySelector('.birth_year_js').innerHTML = birthYear;
 document.querySelector('.age').innerHTML = `${age}`;
 
 const stack = data.info.stack;
